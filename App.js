9adb1a5fef61dd8dbe7c6f5d77b1d11af2b9f362
chewbacca234 <carine.dupuis.es@gmail.com>
2024-02-02 15:39:52 +0100

    app.js with navigation

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
   useFonts,
   LeagueSpartan_900Black,
   LeagueSpartan_500Medium,
   LeagueSpartan_700Bold,
 } from '@expo-google-fonts/league-spartan';
+// AJOUTER LES IMPORTS DES AUTRES SCREENS
+import { Login, SignIn, SignUp } from './screens/login';
+import { GLOBAL_STYLES, COLORS } from './constants';
 
-import { Players } from './screens/storyCreation';
-import Header from './components/Header';
-import { COLORS, GLOBAL_STYLES } from './constants';
-
+// AJOUTER LES IMPORTS DES REDUCEURS
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { user, currentGame } from './reducers';
@@ -19,28 +21,29 @@ const store = configureStore({
   reducer: { user, currentGame },
 });
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
+  let [fontsLoaded, fontError] = useFonts({
     LeagueSpartan_900Black,
     LeagueSpartan_500Medium,
     LeagueSpartan_700Bold,
   });
-
-  if (!fontsLoaded || fontError) {
+  if (!fontsLoaded && !fontError) {
     return null;
   }
 
+  // AJOUTER LES AUTRES STACK.SCREENS DANS LE RETURN
   return (
     <Provider store={store}>
-      <View style={GLOBAL_STYLES.container}>
-        <View style={GLOBAL_STYLES.header}>
-          <Header />
-        </View>
-        <View style={GLOBAL_STYLES.body}>
-          <Players />
-        </View>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <StatusBar hidden={false} />
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
